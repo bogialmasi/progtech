f32c2a1ce0fa7e5b6c8f8dbe86a945c689c6aa12
bogialmasi <boglarkaalmasi@gmail.com>
2023-10-02 17:50:32 +0200

    the lessons of week 4 in a different way

---
+++ week4bank/week4bank/src/main/java/WebBank.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class WebBank implements Bank {
+
+    @Override
+    public BigDecimal getExchangeRate(Currency fromCurrency, Currency toCurrency) {
+        // TODO Implement this web based functionality
+        return null;
+    }
+
+}
